709d0a40a11a72649dd4eae72ee1542777667424
ioloEJ42 <ioloevans42@gmail.com>
2023-08-30 13:51:59 +0100

    Dark Mode

--- JS/script.js
+++ JS/script.js
@@ -30,3 +30,23 @@ function realTimeClock() {
 
 	var t = setTimeout( realTimeClock, 500 );
 }
+// DARK THEME TOGGLE MADE FROM https://lukelowrey.com/css-variable-theme-switcher/
+var toggle = document.getElementById("theme-toggle");
+
+var storedTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
+if (storedTheme)
+    document.documentElement.setAttribute('data-theme', storedTheme)
+
+
+toggle.onclick = function() {
+    var currentTheme = document.documentElement.getAttribute("data-theme");
+    var targetTheme = "light";
+
+    if (currentTheme === "light") {
+        targetTheme = "dark";
+    }
+
+    document.documentElement.setAttribute('data-theme', targetTheme)
+    localStorage.setItem('theme', targetTheme);
+};
+
